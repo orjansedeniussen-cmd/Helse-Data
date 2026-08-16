63b3051d18fd6fcd51463b69f2d38b2e9af626a5
orjansedeniussen-cmd <orjan.sedeniussen@gmail.com>
2026-08-16 09:45:18 +0200

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "pt-app-v2";
+const CACHE = "pt-app-v3";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./pwa-icon.svg"];
 
 self.addEventListener("install", (event) => {
